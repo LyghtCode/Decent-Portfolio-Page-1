2c2f4e84a37052517d61a3af58ada48d2b5291df
LyghtCode <xikago.musica@gmail.com>
2023-03-07 18:37:44 -0600

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -28,12 +28,12 @@ const Navbar = () => {
       <nav className={`${styles.navbar} w-full flex flex-wrap items-center sm:justify-between justify-center`} >
         <div className="flex items-center gap-4">
         <NavItem href="http://decent.xyz/" openInNewTab><Image width={100} height={40} src="/images/decent.png" alt="decent" /></NavItem>
-        <p className="text-white">X</p>
-        <NavItem href="https://www.r3vl.xyz/" openInNewTab><Image width={35} height={35} src="/images/logo-white.svg" alt="reveel" /></NavItem>
+        {/* <p className="text-white">X</p> */}
+        {/* <NavItem href="https://www.r3vl.xyz/" openInNewTab><Image width={35} height={35} src="/images/logo-white.svg" alt="reveel" /></NavItem> */}
         </div>
         <div className="flex items-center gap-4">
           <ConnectButton />
-          <Link href='https://github.com/decentxyz/Portfolio-Page' target='_blank'>
+          <Link href='https://github.com/lyghtcode/' target='_blank'>
             <Image src='/images/github-mark-white.svg' height={20} width={20} alt='link to repository' />
           </Link>
           </div>
